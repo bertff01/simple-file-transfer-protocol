eb23ac63e26558368c2b92d1609dcd2344970b7a
Frederick Bertram <fred.bertram@gmail.com>
2020-06-29 20:13:03 +0800

    Update cli6.c

--- cli6.c
+++ cli6.c
@@ -1,9 +1,4 @@
-/* cli6.c - 	(Topic 11, HX 22/5/1995)
- *		An improved version of "cli5.c". Since TCP does not preserve the message 
- *              boundaries, each message is preceeded by a two byte value which is the
- *              length of the message. 
- * Revised;	06/11/2007
- */
+
 
 #include  <stdlib.h>
 #include  <stdio.h>
